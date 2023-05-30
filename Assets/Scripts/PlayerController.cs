eba28a5f292d0350e5d305075706521e0a07b522
danielforkner <daniel.forkner@gmail.com>
2023-05-30 10:45:34 -0400

    refactor

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,16 +2,37 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] float torqueAmount = 1;
+    [SerializeField] float torqueAmount = 1.5f;
+    [SerializeField] float boostSpeed = 30f;
+    [SerializeField] float baseSpeed = 15f;
     Rigidbody2D rb2d;
+    SurfaceEffector2D ground;
     // Start is called before the first frame update
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        ground = FindObjectOfType<SurfaceEffector2D>();
     }
 
     // Update is called once per frame
     void Update()
+    {
+        RotatePlayer();
+        BoostPlayer();
+    }
+
+    void BoostPlayer()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            ground.speed = boostSpeed;
+        } else 
+        {
+            ground.speed = baseSpeed;
+        }
+    }
+
+    void RotatePlayer()
     {
         if (Input.GetKey(KeyCode.A))
         {
